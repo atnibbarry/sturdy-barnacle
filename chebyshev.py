21844772b2da653ecab47d3c61d6704ca7cb8ae9
Fatoumata Binta BARRY[OGN DCIRE/ODC/SEC] <BARRY110721@orange-sonatel.com>
2024-07-03 18:25:59 +0000

    Commit de la seonde version de la fonction chebyshev_distance

--- chebyshev.py
+++ chebyshev.py
@@ -1,9 +1,4 @@
 def chebyshev_distance(v1, v2):
     if len(v1) != len(v2):
-        raise ValueError("Il faut que les vecteurs ont la même longueur!!!")
-    max_diff = 0
-    for a, b in zip(v1, v2):
-        diff = abs(a - b)
-        if diff > max_diff:
-            max_diff = diff
-    return max_diff
+        raise ValueError("Ils doivent avoir la même longueur")
+    return max(map(lambda a_b: abs(a_b[0] - a_b[1]), zip(v1, v2)))
